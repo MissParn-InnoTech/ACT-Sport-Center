821070cd83f28024d84c7288ed7287bb026d289c
Phitchaphorn PARN <phitchaphornparn@Phitchaphorns-MacBook-Pro-27604.local>
2026-09-17 05:05:27 +0700

    เพิ่มภาพมาสคอตหน้า Login

--- src/App.jsx
+++ src/App.jsx
@@ -457,7 +457,7 @@ function LoginScreen({ loginId, setLoginId, onLogin, err }) {
         }} />
         {/* mascot illustration */}
         <div className="relative flex-1 flex items-end justify-center px-6 pt-10">
-          <img src="https://i.postimg.cc/7ZCLbZ9Q/Art-Work-Sport-Center.png" alt="ACT Sport Center mascots"
+          <img src="https://i.postimg.cc/7YzFwgGY/Untitled-design-(2).png" alt="ACT Sport Center mascots"
             className="max-w-full max-h-full object-contain" style={{ objectPosition: "bottom" }} />
         </div>
         <div className="relative px-10 pb-8">
